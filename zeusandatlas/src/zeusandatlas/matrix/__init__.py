66a69a28cf26b0c67688f7059f3d79bc27c5aa2b
zeusandatlas <zeusandatlas25@gmail.com>
2026-09-12 15:47:25 -0500

    Fix __init__.py content for matrix subpackage, bump to 0.1.2

--- zeusandatlas/src/zeusandatlas/matrix/__init__.py
+++ zeusandatlas/src/zeusandatlas/matrix/__init__.py
@@ -1,5 +1,3 @@
-cat > src/zeusandatlas/matrix/__init__.py << 'EOF'
 from .elementary import rowswap, rowscale, rowreplacement, rref
 
 __all__ = ["rowswap", "rowscale", "rowreplacement", "rref"]
-EOF
